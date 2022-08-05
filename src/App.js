61a16076e4a5ece9f1d5abc96ab574304909db14
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-05 17:48:38 -0400

    Setup Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      Whats upp
-    </div>
+    <Router>
+      <Navbar/>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon,NavMenu, NavItem, NavLinks } from './NavbarElements'
+
+export default function Navbar() {
+
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to='/'>dolla</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
